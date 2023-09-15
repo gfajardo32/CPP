8f73981ce2cf3315a6dca85edca27b43b99f7cb8
ElPapuHN <100800993+ElPapuHN@users.noreply.github.com>
2023-09-15 16:16:57 -0500

    Dog years to human year.

---
+++ dog_years.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+//This version of the program is for dogs older than 2 years old.
+int main() {
+  int dog_age = 10;
+//“The first two years of a dog’s life count as 21 human years.”
+  int early_years = 21;
+//“Each following year counts as 4 human years.”
+  int later_years = (dog_age -2)*4;
+//ads your dog's early years and later years so you can have them in human years.
+  int human_years = early_years + later_years;
+
+  std::cout << "My name is SOMEDOGGO! Ruff ruff, I am " << human_years << " years old in human years.";
+}
